3d6072a0fa14f0c631352581a62130e4a8ffbffd
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-03-02 21:49:34 +0800

    normal update
    add pic res

--- routes/wechatBot.js
+++ routes/wechatBot.js
@@ -21,7 +21,21 @@ var WechatAPI = require('wechat-api');
 var api = new WechatAPI(process.env.AppID,
   process.env.secretKey);
 
-router.use('/', wechat(config.token).text(function(message, req, res, next) {
+router.use('/', wechat(config.token).image(function(message, req, res, next) {
+  // message为图片内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359124971',
+  // MsgType: 'image',
+  // PicUrl: 'http://mmsns.qpic.cn/mmsns/bfc815ygvIWcaaZlEXJV7NzhmA3Y2fc4eBOxLjpPI60Q1Q6ibYicwg/0',
+  // MediaId: 'media_id',
+  // MsgId: '5837397301622104395' }}).voice(function(message, req, res, next) {
+  // TODO
+        res.reply({
+          type: "text",
+          content: '不约，不约，叔叔我们不约！'
+        });
+}).text(function(message, req, res, next) {
   // message为文本内容
   // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
   // CreateTime: '1359125035',
@@ -82,20 +96,6 @@ router.use('/', wechat(config.token).text(function(message, req, res, next) {
          ]);
       break;
   }
-}).image(function(message, req, res, next) {
-  // message为图片内容
-  // { ToUserName: 'gh_d3e07d51b513',
-  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
-  // CreateTime: '1359124971',
-  // MsgType: 'image',
-  // PicUrl: 'http://mmsns.qpic.cn/mmsns/bfc815ygvIWcaaZlEXJV7NzhmA3Y2fc4eBOxLjpPI60Q1Q6ibYicwg/0',
-  // MediaId: 'media_id',
-  // MsgId: '5837397301622104395' }}).voice(function(message, req, res, next) {
-  // TODO
-        res.reply({
-          type: "text",
-          content: '不约，不约，叔叔我们不约！'
-        });
 }).voice(function(message, req, res, next) {
   // message为音频内容
   // { ToUserName: 'gh_d3e07d51b513',
